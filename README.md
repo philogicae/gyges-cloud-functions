2ea3661b9ea2ed4e6020e621b73891785bc0466d
Arnaud Soulier <soulier-arnaud@hotmail.fr>
2020-09-29 02:56:13 +0300

    Initial commit 2

--- README.md
+++
@@ -1,1 +0,0 @@
-gyges-cloud-functions
---
+++ functions/src/index.ts
@@ -0,0 +1,6 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+import functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Cloud Firestore.
+import admin = require('firebase-admin');
+admin.initializeApp();
