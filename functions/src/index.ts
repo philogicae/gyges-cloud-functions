5a6882bb1d24b7e4a9ca0ccc16e6111c1327501d
Arnaud Soulier <soulier-arnaud@hotmail.fr>
2020-09-29 04:42:49 +0300

    Add example

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,3 +4,14 @@ import functions = require('firebase-functions');
 // The Firebase Admin SDK to access Cloud Firestore.
 import admin = require('firebase-admin');
 admin.initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Cloud Firestore under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've succesfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+    });
