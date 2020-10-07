9a30a50dd33331d60426f3a3834c22b5aec4eb13
Arnaud Soulier <soulier-arnaud@hotmail.fr>
2020-10-08 02:45:58 +0300

    Add invitations function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,59 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-import functions = require('firebase-functions');
+// Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+import functions = require("firebase-functions");
 
-// The Firebase Admin SDK to access Cloud Firestore.
-import admin = require('firebase-admin');
+// Firebase Admin SDK to access Cloud Firestore.
+import admin = require("firebase-admin");
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into 
-// Cloud Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
-    const writeResult = await admin.firestore().collection('messages').add({original: original});
-    // Send back a message that we've succesfully written the message
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
-    });
+// Cloud Firestore object
+const db = admin.firestore();
+
+exports.invitations = functions.firestore
+  .document("friends/{userId}")
+  .onUpdate((change, context) => {
+    const uid: String = context.params.userId;
+    const previousList: String[] = change.before.get("friends");
+    const newList: String[] = change.after.get("friends");
+
+    newList
+      .filter((fid: String) => !previousList.includes(fid))
+      .forEach((fid) => {
+        let friends: String[] | undefined;
+        db.doc("friends/" + fid)
+          .get()
+          .then(
+            (doc) => (friends = doc.exists ? doc.get("friends") : undefined)
+          )
+          .catch(() =>
+            functions.logger.error("Access error with friends/${fid}")
+          );
+
+        if (friends !== undefined && !friends.includes(uid)) {
+          const invitationsList = db.doc("invitations/" + fid);
+          invitationsList
+            .get()
+            .then((doc) => {
+              if (doc.exists) {
+                const invitations: String[] = doc.get("invitations");
+                if (!invitations.includes(uid)) {
+                  invitations.push(uid);
+                  invitationsList
+                    .update({ invitations: invitations })
+                    .catch(() =>
+                      functions.logger.error(
+                        "Update error with invitations/${fid}"
+                      )
+                    );
+                  functions.logger.log(
+                    "Friend added for ${uid} - Invitation added for ${fid}"
+                  );
+                }
+              }
+            })
+            .catch(() =>
+              functions.logger.error("Access error with invitations/${fid}")
+            );
+        }
+      });
+    return Promise.resolve();
+  });
