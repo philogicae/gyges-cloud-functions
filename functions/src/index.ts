ccc7482985bf986e11fe39de94d000ffe8c9bac8
Arnaud Soulier <soulier-arnaud@hotmail.fr>
2020-10-08 16:43:32 +0300

    Fix cold boot and performance

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,73 +1,118 @@
-// Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers
-import functions = require("firebase-functions");
-
 // Firebase Admin SDK to access Cloud Firestore
-import admin = require("firebase-admin");
-admin.initializeApp();
+import { firestore as fs_extern, initializeApp } from "firebase-admin";
 
-// Cloud Firestore object
-const db = admin.firestore();
+// Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers
+import { firestore as fs_intern, logger } from "firebase-functions";
 
-// Smarter cold boot
+// Smarter cold boot firebase-admin
+let is_admin_initialized = false;
+const ensureAdminIsInitialized = () => {
+  if (!is_admin_initialized) {
+    initializeApp();
+    is_admin_initialized = true;
+  }
+};
+
+// Smarter cold boot functions
 const function_name = process.env.FUNCTION_NAME || process.env.K_SERVICE;
 
 if (!function_name || function_name === "invitations") {
-  exports.invitations = functions.firestore
+  ensureAdminIsInitialized();
+  const db = fs_extern();
+
+  exports.invitations = fs_intern
     .document("friends/{userId}")
-    .onUpdate((change, context) => {
-      const uid: String = context.params.userId;
+    .onUpdate(async (change, context) => {
       const previousList: String[] = change.before.get("friends");
       const newList: String[] = change.after.get("friends");
 
-      newList
-        .filter((fid: String) => !previousList.includes(fid))
-        .forEach((fid) => {
-          db.doc("friends/" + fid)
+      const uid: String = context.params.userId;
+      const fid: String | undefined = newList
+        .filter((id: String) => !previousList.includes(id))
+        .pop();
+
+      return fid === undefined
+        ? Promise.resolve().then(() =>
+            logger.log("Ignored : Friend removed or no change")
+          )
+        : db
+            .doc("friends/" + fid)
             .get()
-            .then((friendsDoc) => {
-              if (friendsDoc.exists) {
-                const friends: String[] = friendsDoc.get("friends");
-                if (!friends.includes(uid)) {
-                  const invitationsList = db.doc("invitations/" + fid);
-                  invitationsList
-                    .get()
-                    .then((invitationsDoc) => {
-                      if (invitationsDoc.exists) {
-                        const invitations: String[] = invitationsDoc.get(
-                          "invitations"
-                        );
-                        if (!invitations.includes(uid)) {
-                          invitations.push(uid);
-                          invitationsList
-                            .update({ invitations: invitations })
-                            .then(() =>
-                              functions.logger.log(
-                                "Friend added for " +
-                                  uid +
-                                  " - Invitation added for " +
-                                  fid
-                              )
+            .then(async (friendsDoc) =>
+              !friendsDoc.exists
+                ? Promise.resolve().then(() =>
+                    logger.error("Ignored : friends/" + fid + " doesn't exist")
+                  )
+                : async () =>
+                    friendsDoc.get("friends").includes(uid)
+                      ? Promise.resolve().then(() =>
+                          logger.log(
+                            "Ignored : " +
+                              uid +
+                              " already exist in friends/" +
+                              fid
+                          )
+                        )
+                      : db
+                          .doc("invitations/" + fid)
+                          .get()
+                          .then(async (invitationsDoc) =>
+                            !invitationsDoc.exists
+                              ? Promise.resolve().then(() =>
+                                  logger.error(
+                                    "Ignored : invitations/" +
+                                      fid +
+                                      " doesn't exist"
+                                  )
+                                )
+                              : async () => {
+                                  const invitations: String[] = invitationsDoc.get(
+                                    "invitations"
+                                  );
+                                  return invitations.includes(uid)
+                                    ? Promise.resolve().then(() =>
+                                        logger.log(
+                                          "Ignored : " +
+                                            uid +
+                                            " already exist in invitations/" +
+                                            fid
+                                        )
+                                      )
+                                    : async () => {
+                                        invitations.push(uid);
+                                        return db
+                                          .doc("invitations/" + fid)
+                                          .update({
+                                            invitations: invitations,
+                                          })
+                                          .then(() =>
+                                            logger.log(
+                                              "Friend added for " +
+                                                uid +
+                                                " - Invitation added for " +
+                                                fid
+                                            )
+                                          )
+                                          .catch((err) =>
+                                            logger.error(
+                                              "Update error on invitations/" +
+                                                fid +
+                                                ":",
+                                              err
+                                            )
+                                          );
+                                      };
+                                }
+                          )
+                          .catch((err) =>
+                            logger.error(
+                              "Access error on invitations/" + fid + ":",
+                              err
                             )
-                            .catch(() =>
-                              functions.logger.error(
-                                "Update error with invitations/" + fid
-                              )
-                            );
-                        }
-                      }
-                    })
-                    .catch(() =>
-                      functions.logger.error(
-                        "Access error with invitations/" + fid
-                      )
-                    );
-                }
-              }
-            })
-            .catch(() =>
-              functions.logger.error("Access error with friends/" + fid)
+                          )
+            )
+            .catch((err) =>
+              logger.error("Access error on friends/" + fid + ":", err)
             );
-        });
-      return Promise.resolve();
     });
 }
